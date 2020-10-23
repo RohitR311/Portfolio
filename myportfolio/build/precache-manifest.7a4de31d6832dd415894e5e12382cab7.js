1bebfee98d2c8f9c940833da789a62cc59eead72
Rohit <rohit.rajan031101@gmail.com>
2020-10-23 10:02:56 +0530

    v1 commit

---
+++ myportfolio/build/precache-manifest.7a4de31d6832dd415894e5e12382cab7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c1cc1f8c547186c80f20ac4bfa131b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4088f3d685fcf9bca849",
+    "url": "/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "802f3f47d0d1b632c756",
+    "url": "/static/css/main.a50b185f.chunk.css"
+  },
+  {
+    "revision": "4088f3d685fcf9bca849",
+    "url": "/static/js/2.1e5ab9d0.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/static/js/2.1e5ab9d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "802f3f47d0d1b632c756",
+    "url": "/static/js/main.f06d1fb7.chunk.js"
+  },
+  {
+    "revision": "e3473a344493b6ab086d",
+    "url": "/static/js/runtime-main.21e4bbbf.js"
+  }
+]);
